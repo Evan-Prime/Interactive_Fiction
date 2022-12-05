018429357d66957207ef0eb136de46f2f714bd27
Evan MacDonald <evan.t.macdonald01@gmail.com>
2022-12-05 16:32:34 -0400

    Minor Fix

--- Interactive_Fiction/Program.cs
+++ Interactive_Fiction/Program.cs
@@ -8,12 +8,23 @@ namespace Interactive_Fiction
 {
     internal class Program
     {
-        string[] story;
+        static string[] story;
+        
 
         static void Main(string[] args)
         {
             Console.WriteLine("I.F. Story");
             Console.WriteLine();
+
+            //story = System.IO.File.ReadAllLines(@"Story.txt");
+            //Reading Files
+            String Snack = "You are in the woods and see a tree and a river.;You climb the tree.;You cross the river.;2;3";
+            story = Snack.Split(';');
+
+            for (int i = 0; i < story.GetLength(0); i++)
+            {
+                Console.WriteLine(story[i]);
+            }
 
             Console.WriteLine();
             Console.WriteLine("Press any key to exit...");
