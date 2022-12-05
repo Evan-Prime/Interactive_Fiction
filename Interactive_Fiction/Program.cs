a146718309d1d7c82d899a5e58543cdfbea97dc1
Evan MacDonald <evan.t.macdonald01@gmail.com>
2022-12-05 10:57:44 -0400

    Hello World

---
+++ Interactive_Fiction/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interactive_Fiction
+{
+    internal class Program
+    {
+        string[] story;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I.F. Story");
+            Console.WriteLine();
+
+            Console.WriteLine();
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey(true);
+        }
+
+
+    }
+}
