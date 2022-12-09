f64a5096a2cd8de8114b68c4f83cace78ce35b94
Evan MacDonald <evan.t.macdonald01@gmail.com>
2022-12-09 13:52:42 -0400

    Add .txt into the code, and finished the code.
    Also finished up the Story

--- Interactive_Fiction/Program.cs
+++ Interactive_Fiction/Program.cs
@@ -9,8 +9,11 @@ namespace Interactive_Fiction
     internal class Program
     {
         static string[] storyText;
-        
-        
+        static string[] story;
+        static int pageNum = 1;
+        static int reset = 1;
+        static bool inPlay = true;
+
 
         static void Main(string[] args)
         {
@@ -19,46 +22,87 @@ namespace Interactive_Fiction
 
             //story = System.IO.File.ReadAllLines(@"Story.txt");
             //Reading Files
-            string storyText = "You are in the woods and see a tree and a river.;You climb the tree.;You cross the river.;2;3";
-            string[] story = storyText.Split(';');
+            string[] storyTxt = System.IO.File.ReadAllLines(@"Story.txt");
+            //storyText = storyTxt[pageNum].Split('%');
+            storyText = storyTxt;
+            story = storyText[pageNum].Split(';');
 
-            DisplayCurrentPage(story);
+            while (inPlay == true)
+            {
+                DisplayCurrentPage(story);
+                
+            }
             /*
             for (int i = 0; i < story.GetLength(0); i++)
             {
                 Console.WriteLine(story[i]);  
             }
             */
-
-            Console.WriteLine();
-            Console.WriteLine("Press any key to exit...");
-            Console.ReadKey(true);
         }
 
         static void DisplayCurrentPage(string[] story)
         {
-            string MainText = story[0];
-            string FirstChoice = story[1];
-            string SecondChoice = story[2];
-            string FirstPage = story[3];
-            string SecondPage = story[4];
-            Console.Clear();
-            Console.WriteLine(MainText);
-            Console.WriteLine();
-            Console.WriteLine("[Q] - " + FirstChoice + " " + FirstPage);
-            Console.WriteLine("[E] - " + SecondChoice + " " + SecondPage);
-            Console.WriteLine();
-            Console.Write("Choose your option: ");
-            Console.ReadKey(false);
-        }
+            if (story.Length == 1)
+            {
+                string MainText = story[0];
+                Console.Clear();
+                Console.WriteLine(MainText);
+                Console.WriteLine();
+                Console.WriteLine("Press [R] to reset the story.");
+                Console.WriteLine("Press [Esc] to exit.");
+                InputSystem();
+            }
+            else
+            {
+                string MainText = story[0];
+                string FirstChoice = story[1];
+                string SecondChoice = story[2];
 
+                Console.Clear();
+                Console.WriteLine(MainText);
+                Console.WriteLine();
+                Console.WriteLine("[Q] - " + FirstChoice);
+                Console.WriteLine("[E] - " + SecondChoice);
+                Console.WriteLine();
+                Console.Write("Choose your option: ");
+                InputSystem();
+                Console.ReadKey(false);
+            }
+        }
+        
         static void InputSystem()
         {
             ConsoleKeyInfo ChoiceKey;
             ChoiceKey = Console.ReadKey(true);
 
+            if (story.Length == 1)
+            {
+                if (ChoiceKey.Key == ConsoleKey.R)
+                {
+                    story = storyText[reset].Split(';');
+                }
+                else if (ChoiceKey.Key == ConsoleKey.Escape)
+                {
+                    inPlay = false;
+                }
+            }
+            else
+            {
+                string FirstPage = story[3];
+                string SecondPage = story[4];
 
+                if (ChoiceKey.Key == ConsoleKey.Q)
+                {
+                    pageNum = int.Parse(FirstPage);
+                    story = storyText[pageNum].Split(';');
+                }
+                else if (ChoiceKey.Key == ConsoleKey.E)
+                {
+                    pageNum = int.Parse(SecondPage);
+                    story = storyText[pageNum].Split(';');
+                }
+            }
         }
-
+        
     }
 }
